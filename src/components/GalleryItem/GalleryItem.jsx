cf5e80c606a7a14655998564256143f0a51b41c9
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-07-11 10:10:20 -0500

    naming convention

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -5,12 +5,12 @@ function GalleryItem({item, likeClick}){
     const toggleImg = () => setItem (!showItem);
 
     return(
-        <span className='Items'>
+        <div className='Items'>
         {showItem ? <img src={item.path} onClick={toggleImg} /> : <p onClick={toggleImg}>{item.description} </p>}
         <button onClick={() => likeClick(item.id)}>like</button>
         <p>{item.likes} likes</p>
 
-</span>
+</div>
 )
 }
 
